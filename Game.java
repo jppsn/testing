e05d48266b19796c9f6bbb376b1e8898e8fe9b26
anders <ajepp87@gmail.com>
2013-01-04 20:38:04 +0100

    Hej magnus
    sss

---
+++ Game.java
@@ -0,0 +1,16 @@
+public class Game {
+
+   public static void main(String[] args) {
+   
+      System.out.println("This program is a simulation of a simple card game.");
+      System.out.println();
+      
+      Deck U = new Deck();
+      System.out.println();
+      
+      Player player01 = new Player();
+      
+
+
+   }
+}
---
+++ Player.java
@@ -0,0 +1,9 @@
+
+public class Player {
+		
+	public Player(){
+		Deck deck = new Deck();
+	}
+	}
+	
+
